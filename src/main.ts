71c93592aed2abbda8e0c1939fa13973cc98a93d
Arif Khan <khanarif1515@gmail.com>
2024-09-15 01:20:57 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+
+function bootstrap() {
+  bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+  // platformBrowserDynamic().bootstrapModule(AppComponent, appConfig).catch(err => console.log(err));
+  // platformBrowserDynamic().bootstrapModule(AppComponent).catch(err => console.log(err));
+}
+
+if (document.readyState === 'complete') {
+  bootstrap();
+} else {
+  document.addEventListener('DOMContentLoaded', bootstrap);
+}
+
