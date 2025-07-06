a4cad2e3a051d86786c1a087026ab91f9fae7cf7
Arif Khan <khanarif1515@gmail.com>
2025-07-06 18:02:00 +0530

    ssr fix try

--- api/index.js
+++ api/index.js
@@ -1,2 +1,7 @@
-const { app } = require('../dist/mak/server/main.server.mjs');
-module.exports = app;
+// const { app } = require('../dist/mak/server/main.server.mjs');
+// module.exports = app;
+
+export default async function handler(req, res) {
+  const { handler } = await import('../dist/mak/server/main.server.mjs');
+  return handler(req, res);
+}
