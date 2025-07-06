afd593610239cfdeba4cddeb3e0ff47c70a7a7f1
Arif Khan <khanarif1515@gmail.com>
2025-07-06 17:50:02 +0530

    20 ssr vercel deploy

---
+++ api/index.js
@@ -0,0 +1,2 @@
+const { app } = require('../dist/mak/server/main.server.mjs');
+module.exports = app;
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'story/:tag',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'vehicle/:brand/:model',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'vehicle/:brand',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
